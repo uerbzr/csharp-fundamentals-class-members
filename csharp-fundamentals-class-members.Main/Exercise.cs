816c3ce7eed9826d1e3d6c3648b5fc25fc54bd4e
uerbzr <nigel.sibbert@gmail.com>
2023-03-13 17:08:51 +0000

    Add project files.

---
+++ csharp-fundamentals-class-members.Main/Exercise.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace csharp_fundamentals_class_members.Main
+{
+    public class Exercise
+    {
+        // Below is an example of a class member in c#.
+        // Where a class describes a thing, a member describes its attributes.
+        // If Car were a class, some members of it might be colour, transmission, make and model.
+        // The structure of defining a member is <visibility> <data type> <name> = <value>
+        // The below member has a visibility of public, meaning other classes can see and change the value.
+        // It has data type of int, which stands for integer. A numeric value.
+        // Its name can be anything as long as that name isn't already in use.
+        public int answerToTheUltimateQuestion = 42;
+
+
+        //TODO: 1. Create an integer member named age with a value of 32
+        public int age = 33;
+
+
+
+        //TODO: 2. Create a String member named firstName with a value of "Jane"
+
+
+
+        //TODO: 3. Create a boolean member named isProgrammer with a value of true
+
+
+
+        //TODO: 4. Change the value below so that the tests pass. Check the README.md file for instructions on
+        // running and reading tests
+        public int firstNumber = 109;
+
+
+
+        //TODO: 5. Change the value below so that the tests pass
+        public string firstString = "Ruby is to Python what car is to carpet.";
+
+
+
+        //TODO: 6. Change the visibility below so that the tests pass
+        private boolean isVisible = true;
+    }
+}
---
+++ csharp-fundamentals-class-members.Test/ExerciseTest.cs
@@ -0,0 +1,59 @@
+﻿using csharp_fundamentals_class_members.Main;
+using NUnit.Framework;
+
+namespace csharp_fundamentals_class_members.Test
+{
+    public class ExerciseTest
+    {
+        Exercise _exercise;
+
+
+        public ExerciseTest()
+        {
+            this._exercise = new Exercise();
+        }
+
+        [Test]
+        public void shouldAnswerTheUltimateQuestion()
+        {
+            Assert.AreEqual(42, this._exercise.answerToTheUltimateQuestion);
+        }
+
+        [Test]
+        public void shouldBeAged32()
+        {
+            Assert.AreEqual(32, this._exercise.age);
+        }
+
+        [Test]
+        public void shouldBeNamedJane()
+        {
+            Assert.AreEqual("Jane", this._exercise.firstName);
+        }
+
+        [Test]
+        public void shouldBeAProgrammer()
+        {
+            Assert.IsTrue(this._exercise.isProgrammer);
+        }
+
+        [Test]
+        public void firstNumberShouldBe9182()
+        {
+            Assert.AreEqual(9182, this._exercise.firstNumber);
+        }
+
+        [Test]
+        public void firstStringShouldBeSpecific()
+        {
+            Assert.AreEqual("Java is to Javascript what car is to carpet.", this._exercise.firstString);
+        }
+
+        [Test]
+        public void isVisibleShouldBePublic()
+        {
+            
+            Assert.IsTrue(this._exercise.isVisible);
+        }
+    }
+}
